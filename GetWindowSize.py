49dafe206b0c8dad95b3e097de4cdcdc665d7ce2
Marcos Lorhan <marcoslorhanbs@gmail.com>
2020-09-25 18:49:30 -0300

    Function GetWindowSize Added

--- GetWindowSize.py
+++ GetWindowSize.py
@@ -3,7 +3,17 @@ jan = tk.Tk()
 jan.title('New Window')
 jan.geometry('500x500')
 
-
+def GetWinSize():
+     WindowSize = jan.geometry()
+     WindowSize = WindowSize.split('x')
+     width = WindowSize[0]
+     height = str(WindowSize[1])
+     height = height.split('+')
+     height = height[0]
+     print(width)
+     print(height)
+GetWinSize()
+#Recommended use a tk.button to call.
 
 
 jan.mainloop()
